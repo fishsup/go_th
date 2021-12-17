101bc59ea04111b894185095c011dd59fb8e8a35
fishsup <azuremem@gmail.com>
2021-12-17 18:34:27 +0800

    close channel

---
+++ src/ch19/channel_close/channel_close_test.go
@@ -0,0 +1,41 @@
+package channelclose
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func dataProducer(ch chan int, wg *sync.WaitGroup) {
+	go func() {
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+		close(ch)
+		wg.Done()
+	}()
+}
+
+func dataReceiver(ch chan int, wg *sync.WaitGroup) {
+	go func() {
+		for i := 0; i < 11; i++ {
+			if data, ok := <-ch; ok {
+				fmt.Println(data)
+			} else {
+				fmt.Println("channel closed")
+			}
+		}
+		wg.Done()
+	}()
+}
+
+func TestData(t *testing.T) {
+	var wg sync.WaitGroup
+	ch := make(chan int)
+	wg.Add(1)
+	dataProducer(ch, &wg)
+	wg.Add(1)
+	dataReceiver(ch, &wg)
+	wg.Wait()
+	fmt.Println("done")
+}
