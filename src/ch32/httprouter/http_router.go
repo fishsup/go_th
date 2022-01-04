fdd81f94356c308734355f2fafcff380951344b6
fishsup <azuremem@gmail.com>
2022-01-04 21:00:10 +0800

    roa

--- src/ch32/httprouter/http_router.go
+++ src/ch32/httprouter/http_router.go
@@ -1,4 +1,4 @@
-package main1
+package http_router
 
 import (
 	"fmt"
--- src/ch32/httprouter/restful.go
+++ src/ch32/httprouter/restful.go
@@ -1,24 +1,24 @@
-package main
+package http_router
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"github.com/julienschmidt/httprouter"
-)
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"github.com/julienschmidt/httprouter"
+// )
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
-}
+// func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+// 	fmt.Fprint(w, "Welcome!\n")
+// }
 
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-}
+// func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+// 	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+// }
 
-func main() {
-	router := httprouter.New()
-	router.GET("/", Index)
-	router.GET("/hello/:name", Hello)
+// func main() {
+// 	router := httprouter.New()
+// 	router.GET("/", Index)
+// 	router.GET("/hello/:name", Hello)
 
-	log.Fatal(http.ListenAndServe(":8083", router))
-}
+// 	log.Fatal(http.ListenAndServe(":8083", router))
+// }
