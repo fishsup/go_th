a375e9c0256c3b4ca51fb6807cfa58ccd8bef6a0
fishsup <azuremem@gmail.com>
2022-02-09 15:47:56 +0800

    leetcode 24

--- leetcode/linkedlist/206/206_test.go
+++ leetcode/linkedlist/206/206_test.go
@@ -1,1 +1,82 @@
 package _06
+
+import (
+	"fmt"
+	"testing"
+)
+
+//给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：head = [1,2,3,4,5]
+//输出：[5,4,3,2,1]
+//
+//
+// 示例 2：
+//
+//
+//输入：head = [1,2]
+//输出：[2,1]
+//
+//
+// 示例 3：
+//
+//
+//输入：head = []
+//输出：[]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 链表中节点的数目范围是 [0, 5000]
+// -5000 <= Node.val <= 5000
+//
+//
+//
+//
+// 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+//
+//
+// Related Topics 递归 链表 👍 2264 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func (s *ListNode) String() string {
+	return fmt.Sprintf("{Val: %v, Next: %v}", s.Val, s.Next)
+}
+
+func TestReverseList(t *testing.T) {
+	head:= &ListNode{Val: 1}
+	cur := head
+	for i := 2; i < 10; i++ {
+		cur.Next = &ListNode{Val: i}
+		cur = cur.Next
+	}
+	fmt.Printf("%+v\n", head)
+	fmt.Printf("%+v\n", reverseList(head))
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var newHead = new(ListNode)
+	var cur = head
+	for cur != nil {
+		next := newHead.Next
+		newHead.Next = cur
+		cur = cur.Next
+		newHead.Next.Next = next
+	}
+	return newHead.Next
+}
--- leetcode/linkedlist/24/24_test.go
+++ leetcode/linkedlist/24/24_test.go
@@ -1,1 +1,75 @@
 package _4
+//给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：head = [1,2,3,4]
+//输出：[2,1,4,3]
+//
+//
+// 示例 2：
+//
+//
+//输入：head = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：head = [1]
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 链表中节点的数目在范围 [0, 100] 内
+// 0 <= Node.val <= 100
+//
+// Related Topics 递归 链表 👍 1230 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func swapPairs(head *ListNode) *ListNode {
+	newHead:=new(ListNode)
+	prev := newHead
+	cur := head
+	//有两个节点时进行交换
+	for cur != nil && cur.Next !=nil{
+		//头结点指向第二个节点
+		prev.Next = cur.Next
+		cur.Next = nil
+
+		//第二个节点指向第一个节点
+		next := prev.Next.Next
+		prev.Next.Next = cur
+
+		//下次循环前驱结点为cur
+		prev = cur
+		//当前节点为第三个节点
+		cur = next
+	}
+	if cur!= nil && cur.Next==nil {
+		prev.Next = cur
+	}
+	return newHead.Next
+}
+//leetcode submit region end(Prohibit modification and deletion)
