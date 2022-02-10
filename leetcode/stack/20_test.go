422e8c8ed0a13c04385c35e36cb06af4b84ff7db
fishsup <azuremem@gmail.com>
2022-02-10 11:38:18 +0800

    20

---
+++ leetcode/stack/20_test.go
@@ -0,0 +1,78 @@
+package stack
+
+//给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+//
+// 有效字符串需满足：
+//
+//
+// 左括号必须用相同类型的右括号闭合。
+// 左括号必须以正确的顺序闭合。
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：s = "()"
+//输出：true
+//
+//
+// 示例 2：
+//
+//
+//输入：s = "()[]{}"
+//输出：true
+//
+//
+// 示例 3：
+//
+//
+//输入：s = "(]"
+//输出：false
+//
+//
+// 示例 4：
+//
+//
+//输入：s = "([)]"
+//输出：false
+//
+//
+// 示例 5：
+//
+//
+//输入：s = "{[]}"
+//输出：true
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= s.length <= 10⁴
+// s 仅由括号 '()[]{}' 组成
+//
+// Related Topics 栈 字符串 👍 2950 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func isValid(s string) bool {
+	stack := []int32{}
+	for _, charr := range s {
+		if charr == '(' {
+			stack = append(stack, ')')
+		} else if charr == '{' {
+			stack = append(stack, '}')
+		} else if charr == '[' {
+			stack = append(stack, ']')
+		} else {
+			if len(stack) == 0 || stack[len(stack)-1] != charr {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
